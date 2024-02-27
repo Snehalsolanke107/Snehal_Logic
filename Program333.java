c5068d1f64d23eb889756cae8a07ad795ba5222e
Snehal Solanke <152488100+Snehalsolanke107@users.noreply.github.com>
2024-02-27 22:26:54 +0530

    Add files via upload

---
+++ Program333.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+class Program333
+{
+    public static void main(String arg[]) throws Exception
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        String str = "     Hello     ";
+
+        System.out.println("Length of str is : "+str.length());
+
+        sobj.close();
+    }
+}
